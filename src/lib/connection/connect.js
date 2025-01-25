cb03e0b8c21b05c1ad14b6c86c0744f3e87a22ba
ft4bhi <psabhiram100@gmail.com>
2025-01-25 14:06:54 +0530

    connection with postgress is done

---
+++ src/lib/connection/connect.js
@@ -0,0 +1,10 @@
+// src/lib/connection/connect.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
